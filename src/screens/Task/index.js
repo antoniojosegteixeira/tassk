a16a4d4f9c3f8440db23d9de7eb474da8f796ffa
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-14 16:11:27 -0400

    Added task fetch

--- src/screens/Task/index.js
+++ src/screens/Task/index.js
@@ -7,8 +7,32 @@ import {
   TouchableOpacity,
   FlatList,
 } from "react-native";
+import { database } from "../../config/firebase";
+import { collection, onSnapshot } from "firebase/firestore";
 
 export default function Task() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const taskCollection = collection(database, "tasks");
+    const unsubscribe = onSnapshot(taskCollection, (snapshot) => {
+      const list = [];
+      snapshot.forEach((doc) => {
+        list.push({
+          _id: doc.id,
+          title: doc.data().title,
+          description: doc.data().description,
+        });
+      });
+
+      setTasks([...tasks, list]);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  console.log(tasks);
+
   return (
     <View>
       <Text>Task</Text>

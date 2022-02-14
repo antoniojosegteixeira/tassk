8b18b8e47229418b3757cc17d5cab21d6c58ef14
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-14 15:40:28 -0400

    Screen components added

--- App.js
+++ App.js
@@ -3,13 +3,19 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import Task from "./src/screens/Task";
+import NewTask from "./src/screens/NewTask";
+import Details from "./src/screens/Details";
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Task">
         <Stack.Screen name="Task" component={Task} />
+        <Stack.Screen name="NewTask" component={NewTask} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Task/index.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+export default function Task() {
+  return (
+    <View>
+      <Text>Task</Text>
+    </View>
+  );
+}

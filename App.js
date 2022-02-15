93e1e73d9fcd5084831ae106e63aa5831dd8fd32
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-15 02:07:43 -0400

    Added login and register screens

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import Task from "./src/screens/Task";
 import NewTask from "./src/screens/NewTask";
 import Details from "./src/screens/Details";
 import Header from "./src/shared/Header/Header";
+import Login from "./src/screens/Login";
+import Register from "./src/screens/Register";
 import colors from "./src/colors/colors";
 
 const homeOptions = {
@@ -32,9 +34,20 @@ const newTaskOptions = {
   },
 };
 
+const authOptions = {
+  headerStyle: {
+    shadowColor: "transparent",
+    backgroundColor: colors.darkBlue,
+  },
+  headerTintColor: "white",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
+
 const Stack = createStackNavigator();
 
-export default function App() {
+const LoggedRoutes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Cronograma">
@@ -52,6 +65,29 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
+};
+
+const UnloggedRoutes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Registrar">
+        <Stack.Screen name="Login" component={Login} options={authOptions} />
+        <Stack.Screen
+          name="Registrar"
+          component={Register}
+          options={authOptions}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const RootNavigator = () => {
+  return <UnloggedRoutes />;
+};
+
+export default function App() {
+  return <RootNavigator />;
 }
 
 const styles = StyleSheet.create({
---
+++ src/screens/Register/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import colors from "../../colors/colors";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.darkBlue,
+    paddingHorizontal: 20,
+    paddingTop: 15,
+  },
+});
+
+export default styles;

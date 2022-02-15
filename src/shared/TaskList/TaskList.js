e5ab4c3bfe07d9d60c2d0df3e6d4fb2173e28435
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-14 21:04:27 -0400

    Icon position changed

--- src/shared/TaskList/TaskList.js
+++ src/shared/TaskList/TaskList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FlatList, TouchableOpacity, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { FontAwesome } from "@expo/vector-icons";
 import styles from "./styles";
 
 export default function TaskList({ tasks }) {
@@ -8,12 +9,30 @@ export default function TaskList({ tasks }) {
     <>
       {tasks.map((task) => {
         return (
-          <TouchableOpacity onPress={() => console.log("press")} key={task._id}>
-            <LinearGradient colors={["#22293a", "#283040"]} style={styles.item}>
-              <Text style={styles.itemText}>{task.title}</Text>
-              <Text style={styles.time}>9:00</Text>
-            </LinearGradient>
-          </TouchableOpacity>
+          <LinearGradient
+            colors={["#22293a", "#283040"]}
+            style={styles.item}
+            key={task._id}
+          >
+            <TouchableOpacity
+              onPress={() => console.log("press")}
+              style={styles.itemContainer}
+            >
+              <View style={styles.taskIcon}>
+                <FontAwesome name="calendar" size={20} color="#fff" />
+              </View>
+              <View style={styles.text}>
+                <Text style={styles.title}>{task.title}</Text>
+                <Text style={styles.time}>9:00</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => console.log("press")}
+              style={styles.ellipsis}
+            >
+              <FontAwesome name="ellipsis-v" size={20} color="#fff" />
+            </TouchableOpacity>
+          </LinearGradient>
         );
       })}
     </>
--- src/shared/TaskList/styles.js
+++ src/shared/TaskList/styles.js
@@ -3,17 +3,47 @@ import colors from "../../colors/colors";
 
 const styles = StyleSheet.create({
   item: {
-    width: "100%",
-    backgroundColor: colors.mediumBlue,
-    padding: 10,
     borderRadius: 8,
     marginVertical: 5,
+    justifyContent: "center",
   },
-  itemText: {
+  itemContainer: {
+    width: "100%",
+    padding: 13,
+    flexDirection: "row",
+  },
+  text: {
+    marginLeft: 8,
+  },
+  title: {
     color: "white",
     fontSize: 18,
   },
-  time: { color: "white" },
+  time: {
+    color: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  taskIcon: {
+    height: 30,
+    width: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  ellipsis: {
+    position: "absolute",
+    right: 0,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 30,
+    width: 30,
+    marginRight: 10,
+  },
 });
 
 export default styles;
